dd7581a70ef3dbbf84011866d45ececd60c4fa41
주성현 <lawyer923sh@yonsei.ac.kr>
2024-09-10 14:53:23 +0900

    feat/models.py 수정

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,11 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    email = models.EmailField(unique=True)
+    gender_choices = [("m", "남성"), ("f", "여성")]
+    
     nickname = models.CharField(max_length=50, unique=True)
     birth = models.DateField()
-    gender = models.CharField(max_length=10, blank=True, null=True)
+    
+    gender = models.CharField(max_length=1, choices=gender_choices, null=True, blank=True)
+    
+    introduction = models.TextField(default="")
